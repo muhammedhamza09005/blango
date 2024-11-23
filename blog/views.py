b48243428e651da366a1bb00acc96c13d50f3e62
Muhammed Hamza <muhammedhamza09005@email.com>
2024-11-23 18:53:36 +0000

    Muhammed Hamza

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post, Author
+from django.contrib.auth import get_user_model
+
+
+def author_details(author: Author) -> str:
+    if not isinstance(author, get_user_model()):
+        return str()
+
+    return (
+        f"{author.first_name} {author.last_name}".strip()
+        or author.username
+    )
 
 
 def index(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
